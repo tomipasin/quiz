4baa872d5a9103655b85941a9744b8116d127189
tomipasin <tomipasin@gmail.com>
2021-04-28 10:32:59 -0300

    Updates

---
+++ precache-manifest.e311bfdf5080070f64f1134f5205d006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00470ce121dee00739b1e9952d1dd355",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "f83dddb05c4de9c164ae",
+    "url": "/quiz/static/css/main.07fb3004.chunk.css"
+  },
+  {
+    "revision": "34c3fafb4817ce8be14a",
+    "url": "/quiz/static/js/2.71dc2452.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quiz/static/js/2.71dc2452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83dddb05c4de9c164ae",
+    "url": "/quiz/static/js/main.b8f84a86.chunk.js"
+  },
+  {
+    "revision": "175c0a9f90b5dcbf789f",
+    "url": "/quiz/static/js/runtime-main.11d2e18e.js"
+  }
+]);
